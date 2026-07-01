8db19dd2b582f2f532a684a29adeec18749b5d3c
Iucasantos <lucas.torquato@npcventures.com.br>
2026-07-01 14:04:43 -0300

    UPDATE scanner, adicionou leitura da segunda página do excel

--- app/scanner.py
+++ app/scanner.py
@@ -7,5 +7,15 @@ load_dotenv(encoding="utf-8-sig")
 PLANOS = os.getenv("P")
 PLANOS_FAIXAS_PRECO = os.getenv("PFP")
 
-p = pd.read_excel(f"Dados/{PLANOS}")
-pfp = pd.read_excel(f"Dados/{PLANOS_FAIXAS_PRECO}")
+#&===================|incoto|======================
+def validate_scan():
+    p = pd.read_excel(f"Dados/{PLANOS}")
+    pfp = pd.read_excel(f"Dados/{PLANOS_FAIXAS_PRECO}")
+    return p, pfp
+
+#&===================|incopar|======================
+def validate_scan():
+    p = pd.read_excel(f"Dados/{PLANOS}")
+    pfp = pd.read_excel(f"Dados/{PLANOS_FAIXAS_PRECO}", sheet_name=1)
+    return p, pfp
+#&===================|incopar|======================
