7b27d9171cfb53a3555042180463f07b76885335
Iucasantos <lucas.torquato@npcventures.com.br>
2026-07-02 08:37:46 -0300

    ADD susipa como a terceira base de csv concluída

---
+++ app/Logic/susipa.py
@@ -0,0 +1,157 @@
+import os
+import uuid
+import sys
+import pandas as pd
+pasta_raiz = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if pasta_raiz not in sys.path:
+    sys.path.append(pasta_raiz)
+from scanner import susipa_scan
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+p,pfp = susipa_scan()
+
+faixa = pfp.iloc[1:11,0].copy()
+pfp_planos=pfp.iloc[0,2:6]
+pfp_normal = pfp.iloc[1:11,2:6].copy()
+
+nomes=pfp.columns[2:6]
+colunas=pfp.iloc[0,2:6]
+
+principal= pd.DataFrame({
+    "Codigo_Plano":nomes,
+    "Nome": colunas 
+})
+
+principal = principal.reset_index(drop=True)
+
+def trimming(linha):
+    texto = str(linha["Nome"]).strip()
+    Tipo_Cobertura = ""
+
+    if "Nosso Plano" in texto:
+        plano = "Nosso Plano"
+        resto = texto.replace("Nosso Plano","").strip()
+        if "Enf." in resto:
+            local = "Enfermaria"
+            Cobertura = resto.replace("Enf.","").strip()
+            Tipo_Cobertura = "" 
+        elif "Apto." in resto:
+            local = "Apartamento"
+            Cobertura = resto.replace("Apto.","").strip()
+            Tipo_Cobertura = "" 
+        elif "Ambulatorial" in resto:
+            Tipo_Cobertura = "Ambulatorial"
+            Cobertura = resto.replace("Ambulatorial","").strip()
+            local = ""
+
+    elif "Nosso Médico" in texto:
+        plano = "Nosso Médico"
+        resto = texto.replace("Nosso Médico","").strip()
+        
+        if "Enf." in resto:
+            local = "Enfermaria"
+            Cobertura = resto.replace("Enf.","").strip()
+            Tipo_Cobertura = "" 
+        elif "Apto." in resto:
+            local = "Apartamento"
+            Cobertura = resto.replace("Apto.","").strip()
+            Tipo_Cobertura = "" 
+        elif "Ambulatorial" in resto:
+            Tipo_Cobertura = "Ambulatorial"
+            Cobertura = resto.replace("Ambulatorial","").strip()
+            local = ""
+
+    convert = {
+        "Norte/Nordeste":"Regional",
+        "Teresina":"Municipal"
+    }
+
+    Cobertura = convert.get(Cobertura,Cobertura)
+    atendimento=local.strip()
+
+    return pd.Series([plano,atendimento,Tipo_Cobertura,Cobertura])
+
+resultado = principal.apply(trimming,axis=1)
+resultado = resultado.rename(columns={
+    0: "Plano",
+    1: "Acomodacao",
+    2: "Tipo_Cobertura",
+    3: "Cobertura"
+})
+
+resultado["Codigo_Plano"] = principal["Codigo_Plano"]
+
+principal = pd.merge(principal, resultado, on='Codigo_Plano', how='left')
+
+principal["Operadora"] = "Hapvida%"
+principal["Porcentagem_Promocao"] = "15%"
+principal["Descricao_Promocao"] = "15% de desconto nas 3 primeiras mensalidades"
+principal["Promocao_Ativa"] = True
+principal["Ativo"] = True
+principal["Coparticipacao"] = "Parcial"
+principal["Observacao"] = ""
+principal["Id"] = [str(uuid.uuid4()) for _ in range(len(principal))]
+principal["Descricao_Plano"] = "" 
+
+principal["Codigo_Plano"] = principal["Codigo_Plano"].astype(str)
+p["Codigo_Plano"] = p["Codigo_Plano"].astype(str)
+
+colunas_p = ["Codigo_Plano", "Tipo_Rede", "Tipo_Contratacao", "Tipo_Rede_Nacional", "Registro_ANS", "Min_Beneficiarios", "Max_Beneficiarios"]
+p_filtrada = p[colunas_p].drop_duplicates(subset=["Codigo_Plano"], keep="first")
+principal = principal.merge(p_filtrada, on="Codigo_Plano", how="left")
+nao_existe = principal["Registro_ANS"].isna()
+
+principal.loc[nao_existe, "Tipo_Cobertura"] = "Ambulatorial + Hospitalar com Obstetrícia"
+principal.loc[nao_existe, "Min_Beneficiarios"] = 1
+principal.loc[nao_existe, "Tipo_Contratacao"] = "PME"
+principal = principal.fillna("")
+
+limpar_int = lambda val: str(int(float(val))) if (val != "" and pd.notna(val)) else ""
+principal["Min_Beneficiarios"] = principal["Min_Beneficiarios"].apply(limpar_int)
+principal["Max_Beneficiarios"] = principal["Max_Beneficiarios"].apply(limpar_int)
+principal = principal[[
+    "Id",
+    "Operadora", 
+    "Nome", 
+    "Registro_ANS", 
+    "Codigo_Plano", 
+    "Coparticipacao", 
+    "Acomodacao", 
+    "Tipo_Contratacao", 
+    "Cobertura", 
+    "Tipo_Cobertura", 
+    "Tipo_Rede", 
+    "Tipo_Rede_Nacional", 
+    "Observacao", 
+    "Min_Beneficiarios", 
+    "Max_Beneficiarios", 
+    "Ativo", 
+    "Descricao_Plano", 
+    "Promocao_Ativa", 
+    "Porcentagem_Promocao", 
+    "Descricao_Promocao"
+]]
+
+print(principal)
+# #&=======================================================================================================================================
+pfp_normal.insert(0, "Faixa_Etaria", faixa.values)
+pfp_normal.columns = ["Faixa_Etaria"] + list(principal["Codigo_Plano"].unique())
+
+tabela_b_longa = pd.melt(
+    pfp_normal, 
+    id_vars=["Faixa_Etaria"], 
+    var_name="Codigo_Plano", 
+    value_name="Valor"
+)
+tabela_b_longa["Codigo_Plano"] = tabela_b_longa["Codigo_Plano"].astype(str)
+
+tabela_c = pd.merge(
+    principal[["Id", "Codigo_Plano"]],tabela_b_longa,on="Codigo_Plano",how="inner")
+
+tabela_c[['Idade_Min', 'Idade_Max']] = tabela_c['Faixa_Etaria'].str.extract(r'(\d+)\D*(\d*)')
+tabela_c.loc[tabela_c['Idade_Max'] == '', 'Idade_Max'] = '120'
+tabela_c = tabela_c[["Id", "Idade_Min", "Idade_Max", "Valor"]]
+
+print(tabela_c)
